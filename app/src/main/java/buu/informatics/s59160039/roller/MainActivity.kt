fae379f9abbc53bd6dec6ccb8c4c57a1e23c172f
wittayakaew <59160039@go.buu.ac.th>
2019-08-09 18:07:27 +0700

    image change

--- app/src/main/java/buu/informatics/s59160039/roller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160039/roller/MainActivity.kt
@@ -3,6 +3,7 @@ package buu.informatics.s59160039.roller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -19,13 +20,21 @@ class MainActivity : AppCompatActivity() {
     }
     private fun rollDrive(){
         val random = Random().nextInt(6)+1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = random.toString()
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val drableResult = when(random){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drableResult)
         Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
     }
 
     private fun resetRoll(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = 0.toString()
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(R.drawable.empty_dice)
     }
 }
